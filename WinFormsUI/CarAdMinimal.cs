a3db5883bc1e0ace6d2b079a86f64a0570e58a2c
MartynasRaf <m.rafanavicius@gmail.com>
2020-10-08 00:14:32 +0300

    +Simple User Control for Car ad to show with minimal info

---
+++ WinFormsUI/CarAdMinimal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarEngine
+{
+    public partial class CarAdMinimal : UserControl
+    {
+        public CarAdMinimal(string carModel, float price, string engine, string comment)
+        {
+            InitializeComponent();
+
+            this.carModel.Text = carModel;
+            this.price.Text = price.ToString() + "€";
+            this.additionInfo.Text = engine;
+            this.additionInfo.Text += " \"" + comment+ "\"";
+
+        }
+
+
+
+
+    }
+}
--- WinFormsUI/Properties/Resources.Designer.cs
+++ WinFormsUI/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace CarEngine.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap branson_f42c_akcija_f47cn {
+            get {
+                object obj = ResourceManager.GetObject("branson_f42c_akcija_f47cn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
