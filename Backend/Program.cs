b2d074c34920cce5c4242ca32adbfa6b5388985d
Andrius Pukšta <andrius.puksta@gmail.com>
2020-10-09 09:26:20 +0300

    Fix merge conflict?

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace Backend
 {
@@ -6,13 +8,12 @@ namespace Backend
     {
         static void Main()
         {
+            //Console.WriteLine(DateTime.Now.ToString("T"));
             Logger logger = new Logger();
+            //logger.Log(new BackendException("labas", new Exception("ate")));
 
-            FileReader reader = new FileReader(logger);
-            FileWriter writer = new FileWriter(logger);
-
-            CarList carList = new CarList(logger, reader, writer);
-            UserList userList = new UserList(logger, reader, writer);
+            CarList carList = new CarList(logger);
+            UserList userList = new UserList(logger);
 
             User user1 = new User
             {
@@ -57,8 +58,15 @@ namespace Backend
                 //images = new string[] { base64ImageRepresentation }
             };
 
-            carList.AddCar(car1);
-            userList.AddUser(user1);
+            carList.JsonAddCar(JsonSerializer.SerializeToUtf8Bytes<Car>(car1));
+            userList.JsonAddUser(JsonSerializer.SerializeToUtf8Bytes<User>(user1));
+
+            List<Car> car2 = JsonSerializer.Deserialize<List<Car>>(carList.JsonSortBy(SortingCriteria.Price));
+
+            car2[0].Id = 2;
+            //Console.WriteLine(carList.lastCarId);
+            carList.JsonAddCar(JsonSerializer.SerializeToUtf8Bytes<Car>(car2[0]));
+            //Console.WriteLine(carList.lastCarId);
         }
     }
 }
