feecb0f613ac3d0b8d253defdf0083d33f706708
MartynasRaf <m.rafanavicius@gmail.com>
2020-10-14 15:29:02 +0300

    Deleted Test1.sln with project files

--- WinFormsUI/CarAd.cs
+++ WinFormsUI/CarAd.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Test1
+namespace CarEngine
 {
-    public partial class Form2 : Form
+    public partial class CarAd : UserControl
     {
-        public Form2()
+        public CarAd()
         {
             InitializeComponent();
         }
