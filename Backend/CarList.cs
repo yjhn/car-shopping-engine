efa241aaef497e1e177b0fd602074af781b09357
Andrius Pukšta <andrius.puksta@gmail.com>
2020-10-14 16:10:30 +0300

    Add automatic car id assignment and ability to get user ad ids by username

--- Backend/CarList.cs
+++ Backend/CarList.cs
@@ -71,13 +71,10 @@ namespace Backend
         // doesn't add car to the user's ad list
         private void AddCar(Car car)
         {
+            car.Id = lastCarId + 1;
+            lastCarId++;
             carList.Add(car);
             carDataWriter.WriteCarData(car);
-            carDataWriter.WriteCarData(car);
-            if (car.Id > lastCarId)
-            {
-                lastCarId = car.Id;
-            }
         }
 
         // params: Car serialized to JSON
@@ -114,6 +111,19 @@ namespace Backend
                     carDataWriter.DeleteCar(id);
                 }
             }
+        }
+
+        public List<int> GetUserAdIds(string username)
+        {
+            List<int> ids = new List<int>();
+            foreach(Car car in carList)
+            {
+                if (car.UploaderUsername.Equals(username))
+                {
+                    ids.Add(car.Id);
+                }
+            }
+            return ids;
         }
     }
 
