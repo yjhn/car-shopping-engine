f4635fc24965a3f02c61da69574f3ba0b99ed6a6
Andrius Pukšta <andrius.puksta@gmail.com>
2020-10-26 14:52:48 +0200

    Fix missing arguments

--- WinFormsUI/Pages/SearchResultsPage.cs
+++ WinFormsUI/Pages/SearchResultsPage.cs
@@ -28,7 +28,29 @@ namespace CarEngine.Pages
 
         private void sortResultsByCombobox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            resultList.SortBy(, sortResultsAscRadioBtn.Checked);
+            SortingCriteria criteria;
+            switch (sortResultsByCombobox.SelectedItem)
+            {
+                case "upload date":
+                    criteria = SortingCriteria.UploadDate;
+                    break;
+                case "price":
+                    criteria = SortingCriteria.Price;
+                    break;
+                case "date of purchase":
+                    criteria = SortingCriteria.DateOfPurchase;
+                    break;
+                case "total kilometers driven":
+                    criteria = SortingCriteria.TotalKilometersDriven;
+                    break;
+                case "original purchase country":
+                    criteria = SortingCriteria.OriginalPurchaseCountry;
+                    break;
+                default:
+                    criteria = SortingCriteria.UploadDate;
+                    break;
+            }
+            resultList.SortBy(criteria, sortResultsAscRadioBtn.Checked);
 
             // display the sorted results (maybe this isn't needed?)
         }
