da95cb7fe6490b454cbb81521b63d5454a7d41c7
Andrius Pukšta <andrius.puksta@gmail.com>
2020-10-28 09:52:28 +0200

    For add Utilities class to Frontend (it's excluded from project for now, because Visual Studio somehow does not see Image class in System.Drawing)

---
+++ Frontend/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace Frontend
+{
+    // for unknown reason this is not working
+    internal static class Utilities
+    {
+        public static Image Base64ToImg(string base64Image)
+        {
+            //data:image/gif;base64,
+            //this image is a single pixel (black)
+            byte[] bytes = Convert.FromBase64String(base64Image);
+
+            Image image;
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                image = Image.FromStream(ms);
+            }
+            return image;
+        }
+    }
+}
