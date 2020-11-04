651505bdee723da92e30398354c596032d30c94d
Andrius Pukšta <andrius.puksta@gmail.com>
2020-11-04 19:03:54 +0200

    [UI] major overhaul to login / signup screen

--- WinFormsUI/Forms/LoginScreen.Designer.cs
+++ WinFormsUI/Forms/LoginScreen.Designer.cs
@@ -28,86 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginScreen));
-            this.usernameText = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.passwordText = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.emailTextbox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.phoneTextbox = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneTextbox)).BeginInit();
             this.SuspendLayout();
             // 
-            // usernameText
+            // usernameLabel
             // 
-            this.usernameText.AutoSize = true;
-            this.usernameText.BackColor = System.Drawing.Color.Transparent;
-            this.usernameText.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.usernameText.ForeColor = System.Drawing.Color.Yellow;
-            this.usernameText.Location = new System.Drawing.Point(70, 19);
-            this.usernameText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.usernameText.Name = "usernameText";
-            this.usernameText.Size = new System.Drawing.Size(106, 25);
-            this.usernameText.TabIndex = 0;
-            this.usernameText.Text = "Username:";
+            this.usernameLabel.AccessibleDescription = "username label";
+            this.usernameLabel.AccessibleName = "username label";
+            this.usernameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.usernameLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.usernameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.usernameLabel.Location = new System.Drawing.Point(52, 30);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(151, 25);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username:";
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(70, 99);
+            this.passwordTextBox.Location = new System.Drawing.Point(52, 109);
             this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(138, 23);
+            this.passwordTextBox.Size = new System.Drawing.Size(150, 23);
             this.passwordTextBox.TabIndex = 1;
             this.passwordTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PasswordTextBox_KeyDown);
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(70, 46);
+            this.usernameTextBox.Location = new System.Drawing.Point(52, 57);
             this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(138, 23);
+            this.usernameTextBox.Size = new System.Drawing.Size(150, 23);
             this.usernameTextBox.TabIndex = 1;
             this.usernameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UsernameTextBox_KeyDown);
             // 
-            // passwordText
+            // passwordLabel
             // 
-            this.passwordText.AutoSize = true;
-            this.passwordText.BackColor = System.Drawing.Color.Transparent;
-            this.passwordText.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.passwordText.ForeColor = System.Drawing.Color.Yellow;
-            this.passwordText.Location = new System.Drawing.Point(70, 71);
-            this.passwordText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.passwordText.Name = "passwordText";
-            this.passwordText.Size = new System.Drawing.Size(102, 25);
-            this.passwordText.TabIndex = 0;
-            this.passwordText.Text = "Password:";
+            this.passwordLabel.AccessibleDescription = "password label";
+            this.passwordLabel.AccessibleName = "password label";
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
+            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.passwordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.passwordLabel.Location = new System.Drawing.Point(52, 82);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(102, 25);
+            this.passwordLabel.TabIndex = 0;
+            this.passwordLabel.Text = "Password:";
             // 
-            // button1
+            // loginButton
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Window;
-            this.button1.Location = new System.Drawing.Point(11, 137);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 42);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "login";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.loginButton.BackColor = System.Drawing.SystemColors.Window;
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loginButton.Location = new System.Drawing.Point(52, 242);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(2);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(150, 42);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "log in";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.BackColor = System.Drawing.SystemColors.Window;
+            this.signUpButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.signUpButton.Location = new System.Drawing.Point(52, 288);
+            this.signUpButton.Margin = new System.Windows.Forms.Padding(2);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(150, 42);
+            this.signUpButton.TabIndex = 2;
+            this.signUpButton.Text = "sign up";
+            this.signUpButton.UseVisualStyleBackColor = false;
+            this.signUpButton.Click += new System.EventHandler(this.SigUpButton_Click);
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.BackColor = System.Drawing.Color.Transparent;
+            this.emailLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.emailLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.emailLabel.Location = new System.Drawing.Point(52, 134);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(64, 25);
+            this.emailLabel.TabIndex = 0;
+            this.emailLabel.Text = "Email:";
+            this.emailLabel.Visible = false;
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.BackColor = System.Drawing.Color.Transparent;
+            this.phoneLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.phoneLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.phoneLabel.Location = new System.Drawing.Point(52, 186);
+            this.phoneLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(74, 25);
+            this.phoneLabel.TabIndex = 0;
+            this.phoneLabel.Text = "Phone:";
+            this.phoneLabel.Visible = false;
+            // 
+            // emailTextbox
+            // 
+            this.emailTextbox.Location = new System.Drawing.Point(52, 161);
+            this.emailTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailTextbox.Name = "emailTextbox";
+            this.emailTextbox.Size = new System.Drawing.Size(150, 23);
+            this.emailTextbox.TabIndex = 1;
+            this.emailTextbox.Visible = false;
+            this.emailTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UsernameTextBox_KeyDown);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.flowLayoutPanel1.Controls.Add(this.usernameLabel);
+            this.flowLayoutPanel1.Controls.Add(this.usernameTextBox);
+            this.flowLayoutPanel1.Controls.Add(this.passwordLabel);
+            this.flowLayoutPanel1.Controls.Add(this.passwordTextBox);
+            this.flowLayoutPanel1.Controls.Add(this.emailLabel);
+            this.flowLayoutPanel1.Controls.Add(this.emailTextbox);
+            this.flowLayoutPanel1.Controls.Add(this.phoneLabel);
+            this.flowLayoutPanel1.Controls.Add(this.phoneTextbox);
+            this.flowLayoutPanel1.Controls.Add(this.loginButton);
+            this.flowLayoutPanel1.Controls.Add(this.signUpButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(20);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(50, 30, 50, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(246, 346);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // phoneTextbox
+            // 
+            this.phoneTextbox.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.phoneTextbox.Location = new System.Drawing.Point(53, 214);
+            this.phoneTextbox.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.phoneTextbox.Name = "phoneTextbox";
+            this.phoneTextbox.Size = new System.Drawing.Size(149, 23);
+            this.phoneTextbox.TabIndex = 3;
+            this.phoneTextbox.Visible = false;
             // 
             // LoginScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(289, 209);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.passwordText);
-            this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.passwordTextBox);
-            this.Controls.Add(this.usernameText);
+            this.ClientSize = new System.Drawing.Size(246, 346);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(266, 389);
+            this.MinimumSize = new System.Drawing.Size(266, 389);
             this.Name = "LoginScreen";
             this.Text = "Log In";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneTextbox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,12 +223,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label usernameText;
+        private System.Windows.Forms.Label usernameLabel;
         private System.Windows.Forms.TextBox passwordTextBox;
-        private System.Windows.Forms.Label passwordText;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Button signUpButton;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox emailTextbox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.NumericUpDown phoneTextbox;
     }
 }
 
--- WinFormsUI/Forms/LoginScreen.cs
+++ WinFormsUI/Forms/LoginScreen.cs
@@ -1,21 +1,50 @@
-﻿using System;
+﻿using DataTypes;
+using Frontend;
+using System;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Test1
 {
     public partial class LoginScreen : Form
     {
+        private IApi _frontendApi;
+
+        // This property MUST be set for this to work correctly
+        // this may not be needed (it should make designer not set Api value to null)
+        [DefaultValue(null)]
+        public IApi Api
+        {
+            get
+            {
+                return _frontendApi;
+            }
+            set
+            {
+                // _frontendApi can be set only once
+                if (_frontendApi == null || value == null)
+                {
+                    _frontendApi = value;
+                }
+                else
+                {
+                    throw new Exception("Error while setting Api");
+                }
+            }
+        }
+
         public LoginScreen()
         {
             InitializeComponent();
             passwordTextBox.PasswordChar = '*';
-
+            phoneTextbox.Controls.RemoveAt(0);
+            phoneTextbox.ResetText();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void LoginButton_Click(object sender, EventArgs e)
         {
-
-
             //Here we should retrieve userToken from backend, if userToken is null it means, user is not logged in, for this button will just make the user token not null and set UserName. This Button will close the Form
 
             Program.user.Username = usernameTextBox.Text;
@@ -24,11 +53,30 @@ namespace Test1
             this.Close();
         }
 
+        private void SigUpButton_Click(object sender, EventArgs e)
+        {
+
+            if (!loginButton.Visible)
+            {
+                // need to check for bad input
+                User user = new User(usernameTextBox.Text, Convert.ToInt64(phoneTextbox.Text), EncryptPassword(passwordTextBox.Text, usernameTextBox.Text), emailTextbox.Text);
+                _frontendApi.AddUser(user);
+            }
+            else
+            {
+                emailLabel.Visible = true;
+                emailTextbox.Visible = true;
+                phoneLabel.Visible = true;
+                phoneTextbox.Visible = true;
+                loginButton.Visible = false;
+            }
+        }
+
         private void UsernameTextBox_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter)
             {
-                button1.PerformClick();
+                loginButton.PerformClick();
             }
         }
 
@@ -36,7 +84,19 @@ namespace Test1
         {
             if (e.KeyCode == Keys.Enter)
             {
-                button1.PerformClick();
+                loginButton.PerformClick();
+            }
+        }
+
+
+        // method to encrypt password
+        public string EncryptPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = string.Format("{0}{1}", salt, password);
+                byte[] saltedPasswordAsBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                return Convert.ToBase64String(sha256.ComputeHash(saltedPasswordAsBytes));
             }
         }
     }
