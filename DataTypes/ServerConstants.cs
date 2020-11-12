8439bdc62432f4c6cc4d33e1b7f7c96d4c77303d
Lėveris Vytautas <vytautas.leveris@mif.stud.vu.lt>
2020-11-13 00:06:58 +0200

    [Server, Backend] Experimental: Server and Backend binaries added to a remote Windows server. There is no need to run server locally.

--- DataTypes/ServerConstants.cs
+++ DataTypes/ServerConstants.cs
@@ -4,17 +4,20 @@ namespace DataTypes
     {
         public const string Host = "0.0.0.0";
         public const string HostForClients = "localhost";
-        public const string Ip = "127.0.0.1";
+        public const string Ip = "80.209.238.247";
+        //public const string IpLocal = "127.0.0.1";
         public const int Port = 8888;
         public const string HttpVersion = "HTTP/1.1";
         public const string Scheme = "https";
         public const string HeaderSeparator = "\r\n";
         public const int MaxBufferSize = 5000000;
         public const int MaxAttempts = 5000;
-        public const int ServerTimeout = 200;
+        public const int ServerTimeout = 200; //should be excluded
         public const int ClientTimeout = 5000;
-        // constants for SSL certificate
-        public const string CertFileName = @"..\..\..\ssl-certificate.pfx";
+        // constants for SSL certificate should be removed
+        public const string CertFileName = @"ssl-certificate.pfx";
+        public const string CertFileNameLocal = @"..\..\..\ssl-certificate.pfx";
+
         public const string Password = "burbulometodas";
 
     }
